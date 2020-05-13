befa97c495849fe7218815bd2796cc150c484e0e
yjk5309 <yunjookim97@daum.net>
2020-05-13 14:22:53 +0900

    200513 | main html, css 적용

--- alleybookstore/urls.py
+++ alleybookstore/urls.py
@@ -20,7 +20,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^accounts/', include('accounts.urls')),
+    #re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^customer/', include('customer.urls')),
-    re_path(r'^seller/', include('seller.urls')),
+    #re_path(r'^seller/', include('seller.urls')),
 ]
--- customer/urls.py
+++ customer/urls.py
@@ -2,3 +2,7 @@ from django.urls import re_path
 from .splitviews import *
 
 app_name = 'customer'
+
+urlpatterns = [
+    re_path(r'^main/$', MainView, name='main'),
+]
