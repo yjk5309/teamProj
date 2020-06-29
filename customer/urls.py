f77b462b0481121eb9deca8a1630d4d4354d66cc
yjk5309 <yunjookim97@daum.net>
2020-06-29 14:18:09 +0900

    200629 | 카카오 지도 api

--- customer/urls.py
+++ customer/urls.py
@@ -4,7 +4,7 @@ from .splitviews import *
 app_name = 'customer'
 
 urlpatterns = [
-    re_path(r'^main/$', MainView, name='main'),
+    re_path(r'^$', MainView, name='main'),
     re_path(r'^search_store/$', SearchStoreView, name='search_store'),
     re_path(r'^searchBook/$', SearchBookView, name='searchBook'),
     re_path(r'^searchBookResult/$', SearchBookResultView, name='searchBookResult'),
