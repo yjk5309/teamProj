3fe756c6396bf00e7b2ab173088e9b498fd5edb2
yjk5309 <yunjookim97@daum.net>
2020-07-15 16:46:42 +0900

    200715 | url name 수정(김윤주)

--- customer/urls.py
+++ customer/urls.py
@@ -5,9 +5,9 @@ app_name = 'customer'
 
 urlpatterns = [
     re_path(r'^$', MainView, name='main'),
-    re_path(r'^search_store/$', SearchStoreView, name='searchStore'),
-    re_path(r'^searchBook/$', SearchBookView, name='searchBook'),
-    re_path(r'^searchBookResult/$', SearchBookResultView, name='searchBookResult'),
-    re_path(r'^mypage/$', MypageView, name='myPage'),
-    re_path(r'^ajax_get_city/$', AjaxGetCityView, name='ajaxGetCity'),
+    re_path(r'^search_store/$', SearchStoreView, name='search_store'),
+    re_path(r'^searchBook/$', SearchBookView, name='search_book'),
+    re_path(r'^searchBookResult/$', SearchBookResultView, name='search_book_result'),
+    re_path(r'^mypage/$', MypageView, name='mypage'),
+    re_path(r'^ajax_get_city/$', AjaxGetCityView, name='ajax_get_city'),
 ]
