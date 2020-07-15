0b083561ef93f1e07c18c59120daac95dd5f8232
SeongWonYang <yang1762@gmail.com>
2020-07-16 04:11:27 +0900

    200715 | url 수정 (양성원)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,12 @@ from .splitviews import *
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^customerLogin/$', CustomerLoginView, name='customer_login'),
-    re_path(r'^customerRegister/$', CustomerRegisterView, name='customer_register'),
-    re_path(r'^customerInfoModify/$', CustomerInfoModifyView, name='customer_info_modify'),
-    re_path(r'^customerPasswordModify/$', CustomerPasswordModifyView, name='customer_password_modify'),
-    re_path(r'^customerLogout/$', CustomerLogoutView, name='customer_logout'),
-    re_path(r'^customerMypage/$', CustomerMyPageView, name='customer_my_page'),
-    re_path(r'^customerPasswordCheck/$', CustomerPasswordCheckView, name='customer_password_check'),
-    re_path(r'^customerUserDelete/$', CustomerUserDeleteView, name='customer_user_delete'),
+    re_path(r'^login/$', CustomerLoginView, name='customer_login'),
+    re_path(r'^register/$', CustomerRegisterView, name='customer_register'),
+    re_path(r'^info-modify/$', CustomerInfoModifyView, name='customer_info_modify'),
+    re_path(r'^password-modify/$', CustomerPasswordModifyView, name='customer_password_modify'),
+    re_path(r'^logout/$', CustomerLogoutView, name='customer_logout'),
+    re_path(r'^mypage/$', CustomerMyPageView, name='customer_my_page'),
+    re_path(r'^password-check/$', CustomerPasswordCheckView, name='customer_password_check'),
+    re_path(r'^user-delete/$', CustomerUserDeleteView, name='customer_user_delete'),
 ]
