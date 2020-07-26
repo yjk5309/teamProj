fe0ec1d0954fc7f525788cab8f98596dfe8d7156
SeongWonYang <yang1762@gmail.com>
2020-07-26 16:10:26 +0900

    [#2]customer_accounts | 피드백 반영(양성원)

--- customer_accounts/urls.py
+++ customer_accounts/urls.py
@@ -6,10 +6,10 @@ app_name = 'customer_accounts'
 urlpatterns = [
     re_path(r'^login/$', LoginView, name='login'),
     re_path(r'^register/$', RegisterView, name='register'),
-    re_path(r'^user_info/(?P<user_id>\d+)/$', InfoModifyView, name='info_modify'),
+    re_path(r'^user-info/(?P<user_id>\d+)/$', InfoModifyView, name='info_modify'),
     re_path(r'^password/(?P<user_id>\d+)/$', PasswordModifyView, name='password_modify'),
     re_path(r'^logout/$', LogoutView, name='logout'),
-    re_path(r'^user_info/$', InfoManageView, name='info_manage'),
+    re_path(r'^user-info/$', InfoManageView, name='info_manage'),
     re_path(r'^password/$', PasswordCheckView, name='password_check'),
     re_path(r'^secede/$', UserDeleteView, name='user_delete'),
 ]
