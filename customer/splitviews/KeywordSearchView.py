addadc70bb21048fa3e03bb4b7fbc97693b1dad4
yjk5309 <yunjookim97@daum.net>
2020-07-30 13:13:51 +0900

    [#8] 피드백 반영 및 html 수정

--- customer/splitviews/KeywordSearchView.py
+++ customer/splitviews/KeywordSearchView.py
@@ -10,6 +10,12 @@ def KeywordSearchView (request):
 
         namedata = execute_and_get(nameSql)
 
+        idSql = "SELECT id, store_name, address"
+        idSql += " FROM bookstore"
+        idSql += " WHERE address LIKE '%" + keyword + "%'"
+
+        listdata = execute_and_get(idSql)
+
         if len(namedata) == 0:
             messages.error(request, '아직 등록되지 않은 서점입니다.')
             return redirect('customer:search_store')
@@ -25,6 +31,14 @@ def KeywordSearchView (request):
                 bookstores.append(json_row)
             bookstores = json.dumps(bookstores)
 
+            liststores = []
+            for data in listdata:
+                row = {'store_id': data[0],
+                       'store_name': data[1],
+                       'address': data[2],
+                       }
+                liststores.append(row)
+
             strSql = "SELECT province FROM province"
 
             datas = execute_and_get(strSql)
@@ -35,4 +49,5 @@ def KeywordSearchView (request):
                 provinces.append(row)
 
             return render(request, 'search_store.html', {'bookstores': bookstores,
+                                                         'liststores': liststores,
                                                          'provinces': provinces})
