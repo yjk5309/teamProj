b86da7aceead01c76534d46a133bad8536b85590
yjk5309 <yunjookim97@daum.net>
2020-08-14 18:27:45 +0900

    [#15] 피드백 반영 카테고리 페이지 수정 메인 페이지 sql 수정 서점 디테일 sql, html 수정 마이리스트 css 수정

--- customer/splitviews/MyPageView.py
+++ customer/splitviews/MyPageView.py
@@ -5,20 +5,12 @@ def MyPageView(request):
     user = request.user
 
     like_bookSql = "SELECT a.book_isbn, b.book_name, b.book_img " \
-              "FROM like_list as a " \
-              "JOIN book as b " \
-              "ON a.book_isbn = b.isbn " \
-              "where a.user_id=(%s) ORDER BY date desc"
-
-    favorite_storeSql = "SELECT a.bookstore_id, b.store_name, b.bookstore_img " \
-               "FROM favorite_bookstore as a " \
-               "JOIN bookstore as b " \
-               "ON a.bookstore_id = b.id " \
-               "where a.user_id=(%s) ORDER BY date desc"
+                   "FROM like_list as a " \
+                   "JOIN book as b " \
+                   "ON a.book_isbn = b.isbn " \
+                   "where a.user_id=(%s) ORDER BY date desc"
 
     datas = execute_and_get(like_bookSql,(user,))
-
-    datas2 = execute_and_get(favorite_storeSql,(user,))
 
     like_books = []
     for data in datas:
@@ -28,8 +20,17 @@ def MyPageView(request):
                }
         like_books.append(row)
 
+
+    favorite_storeSql = "SELECT a.bookstore_id, b.store_name, b.bookstore_img " \
+                        "FROM favorite_bookstore as a " \
+                        "JOIN bookstore as b " \
+                        "ON a.bookstore_id = b.id " \
+                        "where a.user_id=(%s) ORDER BY date desc"
+
+    datas = execute_and_get(favorite_storeSql,(user,))
+
     favorite_stores = []
-    for data in datas2:
+    for data in datas:
         row = {'store_id': data[0],
                'store_name': data[1],
                'store_img': data[2],
