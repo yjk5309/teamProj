41f52ff16094c41e1f04e2a8db391d4073e9d57d
SeongWonYang <yang1762@gmail.com>
2020-08-24 15:57:40 +0900

    [#21] 임시 commit

--- customer/splitviews/BookBasketDeleteView.py
+++ customer/splitviews/BookBasketDeleteView.py
@@ -3,9 +3,6 @@ from .common import *
 def BookBasketDeleteView(request, book_isbn):
     tab = 'basket'
 
-    book_info = execute_and_get("SELECT book_name, book_img, price, publisher, isbn FROM book WHERE isbn = (%s)",
-                                (book_isbn,))
-
     for i in range(len(request.session['test'])):
         if request.session['test'][i]['isbn'] == book_isbn:
             del request.session['test'][i]
