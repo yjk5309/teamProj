231c0bb709ab8cf46c7d21e99b0cc577b663360f
yjk5309 <yunjookim97@daum.net>
2020-09-03 17:17:09 +0900

    [#24] 장바구니 구매 연결 수정 html, view form 데이터 넘기는 부분 수정

--- customer/splitviews/OrderCreateView.py
+++ customer/splitviews/OrderCreateView.py
@@ -17,29 +17,42 @@ def OrderCreateView (request):
         orderSql = "INSERT INTO order_info(user_id, order_name, " \
                    "order_address, order_p_num, order_email, order_memo) " \
                    "VALUES ((%s),(%s),(%s),(%s),(%s),(%s))"
-
         execute(orderSql, (user_id, name, address, p_num, e_mail, memo,))
+
         idSql = "SELECT id FROM order_info " \
                 "where user_id = (%s) and order_name =(%s) and order_address = (%s) " \
                 "and order_p_num = (%s) and order_email = (%s) and order_memo =(%s) " \
                 "order by buy_date desc limit 1"
         id = execute_and_get(idSql, (user_id, name, address, p_num, e_mail, memo,))
+
         if len(id) != 0:
-            store = request.POST.get("store")
-            book = request.POST.get("book")
-            price = request.POST.get("price")
-            is_ajax = request.POST.get('is_ajax')
-            list = request.POST.getlist('list[]')
+            store = request.POST.getlist("store")
+            book = request.POST.getlist("book")
+            price = request.POST.getlist("price")
 
-            storeIdSql = "SELECT id FROM bookstore where store_name=(%s)"
-            store_id = execute_and_get(storeIdSql, (store,))
+            if len(store) >= 2:
+                for i in range(len(store)):
+                    storeIdSql = "SELECT id FROM bookstore where store_name=(%s)"
+                    store_id = execute_and_get(storeIdSql, (store[i],))
 
-            isbnSql = "SELECT book_isbn FROM book_inven " \
-                      "where book_name = (%s) and store_id = (%s)"
-            isbn = execute_and_get(isbnSql, (book, store_id,))
+                    isbnSql = "SELECT book_isbn FROM book_inven " \
+                              "where book_name = (%s) and store_id = (%s)"
+                    isbn = execute_and_get(isbnSql, (book[i], store_id,))
 
-            listSql = "INSERT INTO order_info_list(order_info_id, store_id, isbn, price) " \
-                      "VALUES ((%s),(%s),(%s),(%s))"
-            execute(listSql, (id, store_id, isbn, price,))
+                    listSql = "INSERT INTO order_products(order_info_id, store_id, isbn, price) " \
+                              "VALUES ((%s),(%s),(%s),(%s))"
+                    execute(listSql, (id, store_id, isbn, price[i],))
+
+            else:
+                storeIdSql = "SELECT id FROM bookstore where store_name=(%s)"
+                store_id = execute_and_get(storeIdSql, (store,))
+
+                isbnSql = "SELECT book_isbn FROM book_inven " \
+                          "where book_name = (%s) and store_id = (%s)"
+                isbn = execute_and_get(isbnSql, (book, store_id,))
+
+                listSql = "INSERT INTO order_products(order_info_id, store_id, isbn, price) " \
+                          "VALUES ((%s),(%s),(%s),(%s))"
+                execute(listSql, (id, store_id, isbn, price,))
 
             return redirect('customer:main')
