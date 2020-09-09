a9caefb1d76b9d107a2ce0084da3331175fb89fa
yjk5309 <yunjookim97@daum.net>
2020-09-10 01:44:37 +0900

    [#31] 주문 내역 페이지 생성

--- customer/splitviews/__init__.py
+++ customer/splitviews/__init__.py
@@ -3,7 +3,6 @@ from .SearchStoreView import *
 from .SearchBookView import *
 from .SearchBookResultView import *
 from .CustomerBasketView import *
-from .CustomerOrderListView import *
 from .MyPageView import *
 from .AjaxGetCityView import *
 from .RegionSearchView import *
