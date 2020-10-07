d5174a8994466ac4d7af3c532c0eb7c22ca20e22
SeongWonYang <yang1762@gmail.com>
2020-10-07 21:59:42 +0900

    [#46] seller | 메인 페이지 구축

--- alleybookstore/urls.py
+++ alleybookstore/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 
     re_path(r'^customer_accounts/', include('customer_accounts.urls')),
     re_path(r'^', include('customer.urls')),
-    #re_path(r'^seller/', include('seller.urls')),
+    re_path(r'^seller/', include('seller.urls')),
 
 ]
---
+++ seller/splitviews/common.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render,redirect,HttpResponse
+from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from customer_accounts.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
+import string
+import random
+import hashlib
+import base64
+from django.contrib.auth.hashers import pbkdf2
+
+import os
+import uuid
+import json
+
+def execute_and_get(sql, data = None) -> tuple :
+    try:
+        cursor = connection.cursor()
+        if data is None:
+            result = cursor.execute(sql,)
+
+        else:
+            result = cursor.execute(sql, data)
+
+        datas = cursor.fetchall()
+        connection.commit()
+
+    except:
+        connection.rollback()
+
+    finally:
+        connection.close()
+
+    return datas
+
+
+def execute(sql, data = None) -> tuple :
+    try:
+        cursor = connection.cursor()
+        if data is None:
+            result = cursor.execute(sql,)
+
+        else:
+            result = cursor.execute(sql, data)
+
+        connection.commit()
+
+    except:
+        connection.rollback()
+
+    finally:
+        connection.close()
