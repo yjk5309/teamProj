b05a6cea83f74674fdcfdc27380c2124094b192e
yjk5309 <yunjookim97@daum.net>
2020-10-27 23:08:35 +0900

    [#49] 피드백 반영 판매자 회원 탈퇴 시 delete되는 데이터 추가 마이페이지 이용 시 서점 등록 안되어있을 경우 등록 페이지로 redirect

--- seller_accounts/splitviews/SellerDeleteView.py
+++ seller_accounts/splitviews/SellerDeleteView.py
@@ -11,8 +11,25 @@ def SellerDeleteView(request):
         user_password = request.POST.get('password')
 
         if user.username == user_id and user.check_password(user_password):
-            storeDeleteSql = "DELETE FROM bookstore WHERE seller_id =(%s)"
-            execute(storeDeleteSql, (user_id,))
+            store_delete_sql = "DELETE FROM bookstore WHERE seller_id =(%s)"
+            execute(store_delete_sql, (user_id,))
+
+            inven_delete_sql = "DELETE FROM book_inven " \
+                               "WHERE store_id = (SELECT id FROM bookstore where seller_id = (%s));"
+            execute(inven_delete_sql, (user_id,))
+
+            likebook_delete_sql = "DELETE FROM like_list " \
+                                  "WHERE store_id = (SELECT id FROM bookstore where seller_id = (%s));"
+            execute(likebook_delete_sql, (user_id,))
+
+            favoritestore_delete_sql = "DELETE FROM favorite_bookstore " \
+                                       "WHERE bookstore_id = (SELECT id FROM bookstore where seller_id = (%s));"
+            execute(favoritestore_delete_sql, (user_id,))
+
+            review_delete_sql = "DELETE FROM review " \
+                                "WHERE store_id = (SELECT id FROM bookstore where seller_id = (%s));"
+            execute(review_delete_sql, (user_id,))
+
             user.delete()
             messages.success(request,'회원탈퇴가 정상적으로 완료되었습니다.')
             return redirect('customer_accounts:login')
--- seller_accounts/splitviews/SellerInfoView.py
+++ seller_accounts/splitviews/SellerInfoView.py
@@ -8,13 +8,18 @@ def SellerInfoView(request):
                "FROM bookstore WHERE seller_id = (%s)"
     data = execute_and_get(storeSql, (user,))
 
-    store = {'store_name': data[0][0],
-             'address': data[0][1],
-             'store_num': data[0][2],
-             'store_img': data[0][3],
-             'store_msg': data[0][4],
-             'store_email': data[0][5],
-             'repre_name': data[0][6],
-             }
+    if len(data) == 0 :
+        messages.success(request, "마이페이지는 서점 등록 후 이용가능합니다.")
+        return redirect('seller_accounts:bookstore_register')
 
-    return render(request, 'seller_info.html', {'store':store, 'tab_is': tab_is})
+    else:
+        store = {'store_name': data[0][0],
+                 'address': data[0][1],
+                 'store_num': data[0][2],
+                 'store_img': data[0][3],
+                 'store_msg': data[0][4],
+                 'store_email': data[0][5],
+                 'repre_name': data[0][6],
+                 }
+
+        return render(request, 'seller_info.html', {'store':store, 'tab_is': tab_is})
