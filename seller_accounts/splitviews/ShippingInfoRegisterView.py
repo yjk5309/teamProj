4de53bbbc67a8e4a70bba3d7eb8dc87926ce0130
yjk5309 <yunjookim97@daum.net>
2020-11-05 15:37:40 +0900

    [#49] 피드백 반영 sql 변수명 수정

--- seller_accounts/splitviews/ShippingInfoRegisterView.py
+++ seller_accounts/splitviews/ShippingInfoRegisterView.py
@@ -17,9 +17,9 @@ def ShippingInfoRegisterView(request):
         send_address = request.POST.get("send_address")
         return_address = request.POST.get("return_address")
 
-        orderSql = "INSERT INTO shipping_info(bookstore_id, bank, account, " \
-                   "shipping_policy, send_address, return_address )" \
-                   "VALUES ((%s),(%s),(%s),(%s),(%s),(%s))"
-        execute(orderSql, (store_id[0][0], bank, account, shipping_policy, send_address, return_address,))
+        info_insert_sql = "INSERT INTO shipping_info(bookstore_id, bank, account, " \
+                          "shipping_policy, send_address, return_address )" \
+                          "VALUES ((%s),(%s),(%s),(%s),(%s),(%s))"
+        execute(info_insert_sql, (store_id[0][0], bank, account, shipping_policy, send_address, return_address,))
         messages.success(request, "배송 정보 등록에 성공하였습니다.")
         return redirect('seller:main')
