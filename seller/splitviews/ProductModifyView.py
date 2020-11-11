e9f95f7edf747c72c415ec93a55536ce700d030d
yjk5309 <yunjookim97@daum.net>
2020-11-12 01:24:48 +0900

    [#54] 책 정보 수정 팝업창 팝업 데이터 처리 수정 중

--- seller/splitviews/ProductModifyView.py
+++ seller/splitviews/ProductModifyView.py
@@ -2,5 +2,11 @@ from .common import *
 
 @login_required
 def ProductModifyView(request):
+    store_id = request.POST.get('store_id')
+    isbn = request.POST.get('isbn')
+    book_sql = "SELECT book_name, inven, price, book_msg " \
+               "FROM book_inven WHERE store_id=(%s) and book_isbn=(%s) "
+    data = execute_and_get(book_sql, (store_id, isbn,))
 
-    return render(request,'product_modify.html')
+
+    return render(request, 'product_modify.html', {})
