f0a0e2f616d3ae5961ad5fcbcc72665ee8b49d0d
SeongWonYang <yang1762@gmail.com>
2020-11-16 18:15:21 +0900

    [#57] customer | 구매확정 BAD 알고리즘 수정

--- customer/splitviews/OrderDecisionView.py
+++ customer/splitviews/OrderDecisionView.py
@@ -1,10 +1,7 @@
 from .common import *
 
 def OrderDecisionView(request, order_num):
-
-    order_status = execute_and_get("SELECT order_status FROM order_products WHERE order_num = (%s)", (order_num,))
-
-    if order_status[0][0] == "배송 완료":
-        execute("UPDATE order_products SET order_status = '구매확정' WHERE order_num = (%s)", (order_num,))
+    execute("UPDATE order_products SET order_status = '구매확정' WHERE order_num = (%s) AND order_status = (%s)",
+            (order_num, "배송 완료"))
 
     return redirect("customer:mypage")
