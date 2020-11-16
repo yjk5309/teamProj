4f52c75e3e0abe31bae4261abc614db66e1e5ad5
SeongWonYang <yang1762@gmail.com>
2020-11-16 18:05:39 +0900

    [#53] seller | 주문 요청페이지 주문정보 중복 제거 주무 상세 모달 -> 복합주문 띄움 수정 일부 BAD 코드 수정

--- seller/splitviews/OrderCheckView.py
+++ seller/splitviews/OrderCheckView.py
@@ -5,19 +5,20 @@ def OrderCheckView(request):
 
     seller_id = execute_and_get("SELECT id FROM bookstore WHERE seller_id=(%s)", (user.username,))
 
-    order_data = execute_and_get("SELECT order_num, purchased_price, order_status FROM order_products WHERE store_id = (%s)",
-                                 (seller_id[0][0],))
+    order_datas = execute_and_get("SELECT DISTINCT order_num, order_status FROM order_products WHERE store_id = (%s) AND order_status = (%s)",
+                                  (seller_id[0][0], "결제 대기중",))
 
     order_info_list = []
-    for order_datas in order_data:
-        buyer_data = execute_and_get("SELECT payment, buy_date FROM order_info WHERE order_num = (%s)", (order_datas[0],))
-        if order_datas[2] == "결제 대기중":
-            row = {'order_num': order_datas[0],
-                   'purchased_price': order_datas[1],
-                   'order_status': order_datas[2],
-                   'buy_date': buyer_data[0][1],
-                   'payment': buyer_data[0][0]}
+    for order_data in order_datas:
+        buyer_data = execute_and_get("SELECT payment, buy_date FROM order_info WHERE order_num = (%s)", (order_data[0],))
+        total_price = execute_and_get("SELECT SUM(purchased_price) FROM order_products WHERE order_num = (%s)", (order_data[0],))
 
-            order_info_list.append(row)
+        row = {'order_num': order_data[0],
+               'order_status': order_data[1],
+               'purchased_price': total_price[0][0],
+               'buy_date': buyer_data[0][1],
+               'payment': buyer_data[0][0]}
+
+        order_info_list.append(row)
 
     return render(request, 'order_check.html', {'order_info_list': order_info_list})
