b13683fd030ae064dc7b7f82c8b1acf7d6d89510
yjk5309 <yunjookim97@daum.net>
2020-12-03 22:11:37 +0900

    [#62] 오류 수정, 월 정산서 완료 db 연결 문제 -> 재부팅 후 오류 해결

--- seller/splitviews/MonthlySettlementView.py
+++ seller/splitviews/MonthlySettlementView.py
@@ -8,54 +8,68 @@ def MonthlySettlementView(request):
     store_id = execute_and_get(id_sql, (user,))
     if request.method == 'GET':
         current_date = datetime.today().strftime('%Y-%m')
-        bank_sql = "SELECT a.order_num, sum(a.purchased_price) " \
+        bank_sql = "SELECT a.order_num, a.purchased_price " \
                    "FROM order_products as a JOIN order_info as b ON a.order_num = b.order_num " \
                    "WHERE a.store_id = (%s) and a.settlement = '정산 완료' " \
-                   "and b.payment = 'bank' and a.delivery_compl_time '%%" + current_date + "%%' " \
-                   "group by a.order_num"
+                   "and b.payment = 'bank' and a.delivery_compl_time like '%%" + current_date + "%%' "
         datas = execute_and_get(bank_sql,(store_id[0][0],))
         bank = []
+        bank_sum = 0
         for data in datas:
             row = {'order_num': data[0],
                    'purchased_price': data[1],}
             bank.append(row)
+            bank_sum += int(data[1])
 
-        card_sql = "SELECT a.order_num, sum(a.purchased_price) " \
+        card_sql = "SELECT a.order_num, a.purchased_price " \
                    "FROM order_products as a JOIN order_info as b ON a.order_num = b.order_num " \
                    "WHERE a.store_id = (%s) and a.settlement = '정산 완료' " \
-                   "and b.payment = 'card' and a.delivery_compl_time '%%" + current_date + "%%' " \
-                   "group by a.order_num"
+                   "and b.payment = 'card' and a.delivery_compl_time like '%%" + current_date + "%%' " 
         datas = execute_and_get(card_sql,(store_id[0][0],))
         card = []
+        card_sum = 0
         for data in datas:
             row = {'order_num': data[0],
                    'purchased_price': data[1],}
             card.append(row)
-        return render(request, 'monthly_settlement.html', {'bank': bank, 'card': card})
+            card_sum += int(data[1])
+
+        sum = int(bank_sum + card_sum)
+        commission = int(sum*0.1)
+        result = int(sum - commission)
+        return render(request, 'monthly_settlement.html', {'bank': bank, 'card': card, 'bank_sum': bank_sum, 'card_sum': card_sum,
+                                                           'sum':sum, 'commission':commission, 'result':result})
 
     else:
         date = request.POST.get('date')
-        bank_sql = "SELECT a.order_num, sum(a.purchased_price) " \
+        bank_sql = "SELECT a.order_num, a.purchased_price " \
                    "FROM order_products as a JOIN order_info as b ON a.order_num = b.order_num " \
                    "WHERE a.store_id = (%s) and a.settlement = '정산 완료' " \
-                   "and b.payment = 'bank' and a.delivery_compl_time '%%" + date + "%%' " \
-                   "group by a.order_num"
-        datas = execute_and_get(bank_sql,(store_id[0][0],))
+                   "and b.payment = 'bank' and a.delivery_compl_time like '%%" + date + "%%' "
+        datas = execute_and_get(bank_sql, (store_id[0][0],))
         bank = []
+        bank_sum = 0
         for data in datas:
             row = {'order_num': data[0],
-                   'purchased_price': data[1],}
+                   'purchased_price': data[1], }
             bank.append(row)
+            bank_sum += int(data[1])
 
-        card_sql = "SELECT a.order_num, sum(a.purchased_price) " \
+        card_sql = "SELECT a.order_num, a.purchased_price " \
                    "FROM order_products as a JOIN order_info as b ON a.order_num = b.order_num " \
                    "WHERE a.store_id = (%s) and a.settlement = '정산 완료' " \
-                   "and b.payment = 'card' and a.delivery_compl_time '%%" + date + "%%' " \
-                   "group by a.order_num"
-        datas = execute_and_get(card_sql,(store_id[0][0],))
+                   "and b.payment = 'card' and a.delivery_compl_time like '%%" + date + "%%' "
+        datas = execute_and_get(card_sql, (store_id[0][0],))
         card = []
+        card_sum = 0
         for data in datas:
             row = {'order_num': data[0],
-                   'purchased_price': data[1],}
+                   'purchased_price': data[1], }
             card.append(row)
-        return render(request, 'monthly_settlement.html', {'bank': bank, 'card': card})
+            card_sum += int(data[1])
+
+        sum = int(bank_sum + card_sum)
+        commission = int(sum*0.1)
+        result = int(sum - commission)
+        return render(request, 'monthly_settlement.html', {'bank': bank, 'card': card, 'bank_sum': bank_sum, 'card_sum': card_sum,
+                                                           'sum':sum, 'commission':commission, 'result':result})
