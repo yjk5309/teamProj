011bf9248ee7f43882f8106b28bd8de5fca06ab7
yjk5309 <yunjookim97@daum.net>
2020-12-17 13:57:49 +0900

    [#69] 코드 수정 답변 달기 모달로 수정 페이지 이동 링크 추가

--- seller/splitviews/ReviewAnswerView.py
+++ seller/splitviews/ReviewAnswerView.py
@@ -4,6 +4,8 @@ from .common import *
 def ReviewAnswerView(request):
     answer = request.POST.get('answer')
     review_id = request.POST.get('review_id')
+
     answer_sql = "UPDATE review SET answer = (%s) WHERE id = (%s)"
     execute(answer_sql, (answer, review_id,))
+
     return redirect('seller:review_manage')
