07ef43d2abbc77e1461fd8ee2b6d9e571c4c54cc
SeongWonYang <yang1762@gmail.com>
2020-12-29 18:31:03 +0900

    [#83] customer | 주문 취소 주문 취소 시, 불필요한 조건 제거

--- customer/splitviews/OrderCancelView.py
+++ customer/splitviews/OrderCancelView.py
@@ -2,7 +2,6 @@ from .common import *
 
 @login_required
 def OrderCancelView(request, order_num):
-    execute("UPDATE order_products SET order_status = '결제취소' WHERE order_num = (%s) AND " +
-            "order_status = (%s) OR order_status = (%s)", (order_num, '결제 대기중', '결제 완료',))
+    execute("UPDATE order_products SET order_status = '결제취소' WHERE order_num = (%s)", (order_num,))
 
     return redirect("customer:mypage")
